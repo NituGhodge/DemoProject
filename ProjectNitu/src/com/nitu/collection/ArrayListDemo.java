c8b72a136aaa073eaa7d3487c916492a5d0c1976
NituGhodge <nitughodge1996@gmail.com>
2022-09-14 12:42:48 +0530

    change to foreach implementation

--- ProjectNitu/src/com/nitu/collection/ArrayListDemo.java
+++ ProjectNitu/src/com/nitu/collection/ArrayListDemo.java
@@ -8,12 +8,10 @@ public class ArrayListDemo {
 
 	public static void main(String[] args) {
 		List<String> list=Arrays.asList("radha","jay","aryan","rina");
-		Iterator<String> itr=list.iterator();
-		while(itr.hasNext()) {
-			System.out.println(itr.next());
-			
+		for(String str:list) {
+			System.out.println(str);
 		}
-
+			
 	}
 
 }
